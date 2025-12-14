754cf9554681115e28b4eae81fe2832430a048af
CODEXMAKER <codexmaker12@gmail.com>
2025-12-14 10:36:03 -0600

    client redeploy 1

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,6 +2,12 @@ import PokemonList from "./pages/home/pokemon_list";
 
 const base_url = process.env.NEXT_PUBLIC_SERVER_URL;
 
+if (!base_url) {
+  console.log(process.env);
+
+  throw new Error("NEXT_PUBLIC_SERVER_URL environment variable is not set");
+}
+
 async function getPokemons() {
   const res = await fetch(`${base_url}`, {
     cache: "no-store",
